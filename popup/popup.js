9bb1fda9f44a94b419bd259640afc735a1d2714f
nichthai <nichthai2004@gmail.com>
2021-03-24 07:44:03 -0500

    fix not working on non-yt; performance

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,5 @@
 const fast = document.getElementById('fast');
+const videlement = document.querySelector("div._3ah6yl5wU0");
 
 fast.onclick = function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -6,7 +7,7 @@ fast.onclick = function() {
       target: { tabId: tabs[0].id },
       function: function() {
         setInterval(() => {
-          document.querySelector("iframe[id^='youtube_player_']").parentNode.parentNode.parentNode.dispatchEvent(new KeyboardEvent('keydown', {
+          videlement.dispatchEvent(new KeyboardEvent('keydown', {
             bubbles: true,
             code: "ArrowRight",
             key: "ArrowRight",
